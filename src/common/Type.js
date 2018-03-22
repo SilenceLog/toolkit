1e7356488cb80efc46572e5f3559f8b1e02b7a1e
ycl <455166539@qq.com>
2018-03-23 03:12:51 +0800

    增加json字符串类型判断

--- src/common/Type.js
+++ src/common/Type.js
@@ -1,61 +1,138 @@
 
+/**
+ * [getType 获取状态值]
+ * @param  {*} v [description]
+ * @return {String}   [description]
+ */
 function getType (v) {
   const s = Object.prototype.toString.call(v)
   return s.match(/\[object (.*?)\]/)[1].toLowerCase()
 }
 
-// ['Undefined', 'Null', 'Object', 'Array', 'String', 'Number', 'Boolean', 'RegExp', 'Function'].forEach( (t) => {
-//   this['is' + t] = (v) => {
-//     return getType(v) === t.toLowerCase()
-//   }
-// })
-
+/**
+ * [isUndefined 是否Undefined]
+ * @param  {*}  v [description]
+ * @return {Boolean}   [description]
+ */
 function isUndefined (v) {
   return getType(v) === 'Undefined'.toLowerCase()
 }
 
+/**
+ * [isNull 是否null]
+ * @param  {*}  v [description]
+ * @return {Boolean}   [description]
+ */
 function isNull (v) {
   return getType(v) === 'Null'.toLowerCase()
 }
 
+/**
+ * [isObject 是否对象]
+ * @param  {*}  v [description]
+ * @return {Boolean}   [description]
+ */
 function isObject (v) {
   return getType(v) === 'Object'.toLowerCase()
 }
 
+/**
+ * [isArray 是否数组]
+ * @param  {*}  v [description]
+ * @return {Boolean}   [description]
+ */
 function isArray (v) {
   return Array.isArray ? Array.isArray(v) : getType(v) === 'Array'.toLowerCase()
 }
 
+/**
+ * [isString 是否字符串类型]
+ * @param  {*}  v [description]
+ * @return {Boolean}   [description]
+ */
 function isString (v) {
   return getType(v) === 'String'.toLowerCase()
 }
 
+/**
+ * [isNumber 是否数字]
+ * @param  {*}  v [description]
+ * @return {Boolean}   [description]
+ */
 function isNumber (v) {
   return getType(v) === 'Number'.toLowerCase()
 }
 
+/**
+ * [isBoolean 判断是否布尔值]
+ * @param  {*}  v [description]
+ * @return {Boolean}   [description]
+ */
 function isBoolean (v) {
   return getType(v) === 'Boolean'.toLowerCase()
 }
 
+/**
+ * [isRegExp 是否正则表达式]
+ * @param  {*}  v [description]
+ * @return {Boolean}   [description]
+ */
 function isRegExp (v) {
   return getType(v) === 'RegExp'.toLowerCase()
 }
 
+/**
+ * [isFunction 是否Function]
+ * @param  {*}  v [description]
+ * @return {Boolean}   [description]
+ */
 function isFunction (v) {
   return getType(v) === 'Function'.toLowerCase()
 }
 
+/**
+ * [isNaN 是否NaN]
+ * @param  {*}  v [description]
+ * @return {Boolean}   [description]
+ */
 function isNaN (v) {
   return Number.isNaN ? Number.isNaN(v) : v !== v
 }
 
+/**
+ * [isDOMElement 是否dom]
+ * @param  {*}  o [description]
+ * @return {Boolean}   [description]
+ */
 function isDOMElement (o) {
   return !!(o && typeof window !== 'undefined' && (o === window || o.nodeType))
 }
 
+/**
+ * [isPromise 是否promise]
+ * @param  {*}  v [description]
+ * @return {Boolean}   [description]
+ */
 function isPromise (v) {
   return isObject(v) && isFunction(v.then)
 }
 
-export {getType, isUndefined, isNull, isObject, isArray, isString, isNumber, isBoolean, isRegExp, isFunction, isNaN, isDOMElement, isPromise}
+/**
+ * [isJSON 是否json字符串]
+ * @param  {*}  str [description]
+ * @return {Boolean}     [description]
+ */
+function isJSON (str) {
+  if (typeof str == 'string') {
+    try {
+      const obj = JSON.parse(str)
+      return typeof obj == 'object' && obj
+    } catch (e) {
+      console.log('error:' + str + '!' + e)
+      return false
+    }
+  }
+  console.log('It is not a string!')
+}
+
+export {getType, isUndefined, isNull, isObject, isArray, isString, isNumber, isBoolean, isRegExp, isFunction, isNaN, isDOMElement, isPromise, isJSON}
